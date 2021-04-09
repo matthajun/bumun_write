0c13ccd717834ff1aed435cdbc7e96df6b3082a9
matthajun <thajun@naver.com>
2021-04-09 10:21:33 +0900

    4월9일 작업, 수정

---
+++ models/KDN_LGSYS_L001_RESULT.js
@@ -0,0 +1,33 @@
+const Sequelize = require('sequelize');
+
+module.exports = class KDN_LGSYS_L001_RESULT extends Sequelize.Model {
+    static init(sequelize) {
+        return super.init({
+            message_id: {
+                type: Sequelize.STRING(10),
+                allowNull: true,
+            },
+            res_code: {
+                type: Sequelize.STRING(20),
+                allowNull: true,
+            },
+            res_msg: {
+                type: Sequelize.STRING(30),
+                allowNull: true,
+            },
+            date_time: {
+                type: Sequelize.STRING(30),
+                allowNull: true,
+            },
+        }, {
+            sequelize,
+            timestamps: false,
+            underscored: false,
+            modelName: 'KDN_LGSYS_L001_RESULT',
+            tableName: 'kdn_lgsys_L001_result',
+            paranoid: true,
+            charset: 'utf8',
+            collate: 'utf8_general_ci',
+        });
+    }
+};
